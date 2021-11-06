993f8100e078eae34e812c6cc6e900a107ee2458
sergiosalguero <salgueroflor1982@gmail.com>
2021-11-06 15:18:01 +0100

    Seller screen

--- frontend/src/screens/SellerScreen.js
+++ frontend/src/screens/SellerScreen.js
@@ -2,11 +2,21 @@ import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { listProducts } from "../actions/productActions";
 import { detailsUser } from "../actions/userAction";
+import LoadingBox from "../components/LoadingBox";
+import MessageBox from "../components/MessageBox";
 
 export default function SellerScreen(props) {
   const sellerId = props.match.params.id;
   const userDetails = useSelector((state) => state.userDetails);
   const { loading, error, user } = userDetails;
+
+  const productList = useSelector((state) => state.productList);
+  const {
+    loading: loadingProducts,
+    error: errorProducts,
+    products,
+  } = productList;
+
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(detailsUser(sellerId));
@@ -14,7 +24,21 @@ export default function SellerScreen(props) {
   }, [dispatch, sellerId]);
   return (
     <div className="row top">
-      <div className="col-1"></div>
+      <div className="col-1">
+        {loading ? (
+          <LoadingBox></LoadingBox>
+        ) : error ? (
+          <MessageBox variant="danger">{error}</MessageBox>
+        ) : (
+          <ul className="card card-body">
+            <li>
+              <div>
+                <img src={user.seler.logo} alt={user.seller.name}></img>
+              </div>
+            </li>
+          </ul>
+        )}
+      </div>
       <div className="col-3"></div>
     </div>
   );
